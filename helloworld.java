bdb13c2198109e53394d6e71532c893f6fd49435
Mohit Rangari <118251961+mohitrangari@users.noreply.github.com>
2023-07-15 15:42:19 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -1,6 +1,6 @@
 class Simple{  
     public static void main(String args[]){  
-     System.out.println("Hello batch30 team with mohit , rushwanth  and team");
-      System.out.println("Hello Batch batch30 WITH FOR STUDENTS RUNNING in front of lion and Anita and suchandra sindhu in making CI automated vikas teach devops CI proecss  ");
+     System.out.println("Hello there");
+      System.out.println("hie this is mohit how are u  ");
     }  
 } 
